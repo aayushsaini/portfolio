14e34e61f2e614721d36f96efb75a717409d04a1
Aayush Saini <31177925+aayushsaini@users.noreply.github.com>
2024-11-10 13:01:42 +0530

    UI update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <div className="min-h-screen bg-[#0a0f0d] text-gray-300 font-mono">
       <div className="max-w-4xl mx-auto px-6 py-8">
         <Navigation />
-        <header className="mt-[15vh] mb-16">      
+        <header className="mt-[13vh] mb-16">      
             <h2 className="text-3xl font-bold mb-6 text-white">
             {getGreeting()}, there! 👋
             </h2>
@@ -27,15 +27,15 @@ function App() {
           </p>
           
           <div className="flex flex-wrap gap-4">
-            <a href="mailto:aayushs99@icloud.com" className="flex items-center gap-2 bg-gray-800/50 hover:outline-green-600 hover:outline  px-4 py-2 transition-colors">
+            <a href="mailto:aayushs99@icloud.com" className="flex items-center gap-2 bg-gray-800/50 hover:bg-green-900 hover:outline-green-600 hover:outline  px-4 py-2 transition-colors">
               <img src="https://img.icons8.com/?size=100&id=X0mEIh0RyDdL&format=png&color=000000" alt="Email Icon" width={18} height={18} />
               Send an email
             </a>
-            <a href="https://linkedin.com/in/aayush-saini" className="flex items-center gap-2 bg-gray-800/50 hover:outline-green-600 hover:outline  px-4 py-2 transition-colors">
+            <a href="https://linkedin.com/in/aayush-saini" className="flex items-center gap-2 bg-gray-800/50 hover:bg-green-900 hover:outline-green-600 hover:outline  px-4 py-2 transition-colors">
               <img src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000" alt="LinkedIn Icon" width={18} height={18} />
               /in/aayush-saini
             </a>
-            <a href="https://github.com/aayushsaini" className="flex items-center gap-2 bg-gray-800/50 hover:outline-green-600 hover:outline px-4 py-2 transition-colors">
+            <a href="https://github.com/aayushsaini" className="flex items-center gap-2 bg-gray-800/50 hover:bg-green-900 hover:outline-green-600 hover:outline px-4 py-2 transition-colors">
               <img src="https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=EBEBEB" alt="GitHub Icon" width={18} height={18} />
               /aayushsaini
             </a>
